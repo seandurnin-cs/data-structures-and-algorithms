b6d70dee9ea5a214a3479555e709a057005def61
seandurnin-cs <sdurnin144@gmail.com>
2026-02-25 18:59:43 +0000

    WIP: reverse linked list (iterative/recursive attempt)

---
+++ leetcode/linked-lists/ListNode.java
@@ -0,0 +1,61 @@
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head.next;
+        
+        if(temp.next == null) {
+            head.next = head;
+            return head.next;
+        }
+
+        head.next = head;
+        return reverseList(temp.next);
+    }
+}
+
+class Driver {
+    static ListNode fromArray(int... a) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        for (int x : a) {
+            tail.next = new ListNode(x);
+            tail = tail.next;
+        }
+        return dummy.next;
+    }
+
+    static void printList(ListNode head) {
+        for (ListNode cur = head; cur != null; cur = cur.next) {
+            System.out.print(cur.val);
+            if (cur.next != null)
+                System.out.print(" -> ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = fromArray(1, 2, 3, 4, 5);
+        System.out.print("before: ");
+        printList(head);
+
+        ListNode rev = new Solution().reverseList(head);
+        System.out.print("after:  ");
+        printList(rev);
+    }
+}
