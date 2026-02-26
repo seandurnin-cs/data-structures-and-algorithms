876036a68c00571506ac51fd3cfe8373f5ee2b20
seandurnin-cs <sdurnin144@gmail.com>
2026-02-26 17:40:36 +0000

    setup test harness

--- leetcode/hashmap/0001-two-sum-inefficient/SolutionBruteForce.java
+++ leetcode/hashmap/0001-two-sum-inefficient/SolutionBruteForce.java
@@ -1,9 +1,16 @@
-class Solution {
+import java.util.Scanner;
+public class SolutionBruteForce {
     public int[] twoSum(int[] nums, int target) {
-        
+        return nums;
     }
 
     public static void main(String args[]) {
-        
+        Scanner uI = new Scanner(System.in);
+        int[] test = new int[2];
+        for(int i = 0; i < 2; i++) {
+            test[i] = uI.nextInt();
+            System.out.print(test[i]);
+        }
+        uI.close();
     }
 }
