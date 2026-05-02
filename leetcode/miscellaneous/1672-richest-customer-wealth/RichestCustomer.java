a05e75573d9d1f9ea251c1f22baa7ee1b2d50e94
seandurnin-cs <sdurnin144@gmail.com>
2026-05-02 22:40:37 +0100

    Completed Solution O(MxN)

--- leetcode/miscellaneous/1672-richest-customer-wealth/RichestCustomer.java
+++ leetcode/miscellaneous/1672-richest-customer-wealth/RichestCustomer.java
@@ -1,9 +1,22 @@
 public class RichestCustomer {
     public static void main(String args[]) {
-        int[][] accounts = {{1,2,3},{3,2,1}};
+        int[][] accounts = {{1,5},{7,3},{3,5}};
+        int result = maximumWealth(accounts);
+        System.out.println(result);
     }
 
     public static int maximumWealth(int[][] accounts) {
-        return 0;
+        int temp = 0;
+        int biggest = 0;
+        for(int i = 0; i < accounts.length; i++) {
+            for(int j = 0; j < accounts[i].length; j++) {
+                temp += accounts[i][j];
+            }
+            if(temp > biggest) {
+                biggest = temp;
+            }
+            temp = 0;
+        }
+        return biggest;
     }
 }
