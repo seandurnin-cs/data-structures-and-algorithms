b2791f58ba4ab43a3f4c6d43d9a61bd470481dfd
Sean Durnin <sdurnin144@gmail.com>
2026-05-02 19:43:09 +0100

    Beagin Solution

---
+++ leetcode/miscellaneous/1672-richest-customer-wealth/RichestCustomer.java
@@ -0,0 +1,9 @@
+public class RichestCustomer {
+    public static void main(String args[]) {
+        int[][] accounts = {{1,2,3},{3,2,1}};
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+        return 0;
+    }
+}
