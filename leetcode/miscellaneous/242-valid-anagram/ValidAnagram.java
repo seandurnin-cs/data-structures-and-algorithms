9adaac32a0d90cb3588b4dd1a2d6d0caf770bcb6
seandurnin-cs <sdurnin144@gmail.com>
2026-05-04 19:25:45 +0100

    Improved Best Case with early length check

--- leetcode/miscellaneous/242-valid-anagram/ValidAnagram.java
+++ leetcode/miscellaneous/242-valid-anagram/ValidAnagram.java
@@ -8,6 +8,7 @@ public class ValidAnagram {
     }
 
     public static boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
         char[] sChar = s.toCharArray();
         char[] tChar = t.toCharArray();
         Arrays.sort(sChar);
